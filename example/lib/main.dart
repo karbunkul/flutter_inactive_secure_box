578766b1fedd0c7e5d0b8d0ec2e1c822f31ef953
karbunkul <karbunkul@yourtask.ru>
2022-01-08 13:16:39 +0500

    init commit

---
+++ example/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:secret_resume_view/secret_resume_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      builder: (_, child) => SecretResumeView(child: child!),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('SecretResumeView'),
+      ),
+      body: const Center(
+        child: SecretResumeView(
+          child: Text('Secret'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          showAboutDialog(context: context);
+        },
+      ),
+    );
+  }
+}
---
+++ lib/secret_resume_view.dart
@@ -0,0 +1,3 @@
+library secret_resume_view;
+
+export 'src/secret_resume_view.dart';
